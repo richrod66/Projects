edc7fcb0ee88e2680ce2b22e90728d79e5dca7e8
richrod66 <richrod66@users.noreply.github.com>
2025-10-23 23:15:00 -0400

    First test.

--- rivi-soft-automation/src/test/java/com/rivi/automation/Test.java
+++ rivi-soft-automation/src/test/java/com/rivi/automation/Test.java
@@ -1,7 +1,35 @@
 package com.rivi.automation;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Test {
     public static void main(String[] args) {
-       System.out.println("I am becoming one of the top test automation engineers in the world!");  
+      
+      //Using Chrome Driver
+        WebDriver chromeDriver = new ChromeDriver();
+
+      //Using Edge Driver  
+        WebDriver edgeDriver = new EdgeDriver();
+
+      //Using Firefox Driver
+        WebDriver firefoxDriver = new FirefoxDriver();  
+
+        //chromeDriver.get("https://mmhcloud.com/gateway/");
+        chromeDriver.navigate().to("https://mmhcloud.com/gateway/");
+        edgeDriver.get("https://mmhcloud.com/gateway/");
+        firefoxDriver.get("https://mmhcloud.com/gateway/");
+
+        System.out.println("Title: " + edgeDriver.getTitle());
+        System.out.println("Title: " + firefoxDriver.getTitle());
+        System.out.println("Title: " + chromeDriver.getTitle());
+
+        // Close the browsers
+        chromeDriver.quit();
+        edgeDriver.quit();
+        firefoxDriver.quit();
     }
 }
+    
