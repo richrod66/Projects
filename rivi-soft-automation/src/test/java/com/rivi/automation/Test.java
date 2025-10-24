cb9aedadb67f109c10224e361d3dfa8d81b3a4c6
richrod66 <richrod66@users.noreply.github.com>
2025-10-23 21:05:57 -0400

    New Selenium Project Setup

---
+++ rivi-soft-automation/src/test/java/com/rivi/automation/Test.java
@@ -0,0 +1,7 @@
+package com.rivi.automation;
+
+public class Test {
+    public static void main(String[] args) {
+       System.out.println("I am becoming one of the top test automation engineers in the world!");  
+    }
+}
