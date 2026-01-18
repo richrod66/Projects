a155793c7b3d4c7fd670528eb179833f60c98da3
richrod66 <richrod66@users.noreply.github.com>
2026-01-17 21:19:05 -0500

    Updated files

--- Chroma-Tech-Test-Automation/src/test/java/com/chromatech/GoogleSignInTest.java
+++ Chroma-Tech-Test-Automation/src/test/java/com/chromatech/GoogleSignInTest.java
@@ -1,8 +1,5 @@
 package com.chromatech;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import com.chromatech.utils.TestProperties;
 import com.chromatech.utils.WebDriverUtils;
 
@@ -20,26 +17,6 @@ public class GoogleSignInTest {
             WebDriverUtils.driver.get(TestProperties.BASE_URL);
             System.out.println("Navigated to: " + TestProperties.BASE_URL);
             
-            // Wait for and click Sign In button
-            WebElement signInButton = WebDriverUtils.wait.until(
-                ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@aria-label, 'Sign in')]"))
-            );
-            signInButton.click();
-            System.out.println("Clicked Sign In button");
-            
-            // Wait for email field and enter email
-            WebElement emailField = WebDriverUtils.wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='email']"))
-            );
-            emailField.sendKeys(TestProperties.TEST_EMAIL);
-            System.out.println("Entered email: " + TestProperties.TEST_EMAIL);
-            
-            // Wait for and click Next button
-            WebElement nextButton = WebDriverUtils.wait.until(
-                ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='button' and .//span[text()='Next']]"))
-            );
-            nextButton.click();
-            System.out.println("Clicked Next button");
             
             // Wait a moment to see the result
             Thread.sleep(3000);
