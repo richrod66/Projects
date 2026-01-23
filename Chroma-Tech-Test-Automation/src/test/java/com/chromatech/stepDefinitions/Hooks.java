07b74f4cf3e08b584a20e0c8aa6fb492e83815ce
richrod66 <richrod66@users.noreply.github.com>
2026-01-22 22:31:44 -0500

    Fixed Java path issue

--- Chroma-Tech-Test-Automation/src/test/java/com/chromatech/stepDefinitions/Hooks.java
+++ Chroma-Tech-Test-Automation/src/test/java/com/chromatech/stepDefinitions/Hooks.java
@@ -1,20 +1,24 @@
 package com.chromatech.stepDefinitions;
 
+import java.time.Instant;
+
 import com.chromatech.utils.WebDriverUtils;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 public class Hooks {
 
     @Before
-    public void ssetup() {
+    public void setup(Scenario scenario) {
         // Any setup code if needed
+        scenario.log("Setting up the browser for the test scenario." + Instant.now());
         WebDriverUtils.initBrowser();
     }       
 
     @After
-    public void teardown() {
+    public void teardown(Scenario scenario) {
         // Any teardown code if needed
         WebDriverUtils.tearDown();
     }   
